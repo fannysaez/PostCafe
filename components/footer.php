c36c8b2426464e0713419b2fbf686ea88c13d483
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-25 15:11:13 +0100

    initiale commit

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,27 @@
-<footer>
+<!-- page footer -->
+<footer class="mt-auto">
+    <div class="footer bg-light px-2">
+        <div class="text-center d-flex justify-content-around">
 
-<h1>footer</h1>
+            <p class="mb-0">
+                <i class="bi bi-geo-alt text-primary fs-3"></i><br><br>
+                123 Rue de l'Océan,<br>75001 Ville Natation<br>
+            </p>
+            <p class="mb-0">
+                <i class="bi bi-telephone text-primary fs-3"></i><br><br>
+                <a href="tel:+33123456789">+33 1 23 45 67 89</a><br>
+            </p>
 
+            <p class="mb-0">
+                <i class="bi bi-envelope text-primary fs-3"></i><br><br>
+                <a type="button" href="/pages/contact.php" class="btn-primary-link">M'écrire</a>
+            </p>
+        </div>
+    </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
