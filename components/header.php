79cd60ed7dd005e6206ca596a5d9e6b79ed900d0
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-25 16:39:31 +0100

    initiale commit

--- components/header.php
+++ components/header.php
@@ -11,6 +11,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100"> 
 
     <?php include './components/nav.php'; ?>
