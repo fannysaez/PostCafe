5c93751c059e8a502f2951f70a6208a587f2d51f
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-26 10:33:36 +0100

    initiale commit

--- components/header.php
+++ components/header.php
@@ -8,9 +8,10 @@
     <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </head>
 
-<body class="d-flex flex-column min-vh-100"> 
+<body class="d-flex flex-column min-vh-100">
 
     <?php include './components/nav.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -4,9 +4,22 @@
 <main class="d-flex flex-column align-items-center my-5">
     <img class="PostCafe mb-2" src="/assets/logo/PostCafe.svg" alt="Logo PostCafé" width="150">
     <h1 class="text-center">Bienvenue sur ce blog</h1>
-</main>
+</main><br><br>
+
+
+
+
+
 
 <?php include './components/slides.php'; ?>
+
+
+
+
+
+
+
+
 
 
 <?php include './components/footer.php'; ?>
