b4b4bdef60a96aafd3c0f8ce24370227dfd8fde7
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-27 08:28:09 +0100

    initiale PostCoffee - blog

--- components/card-article.php
+++ components/card-article.php
@@ -1,9 +1,9 @@
-<div class="container-fluid py-2 mb-5">
-    <div class="row justify-content-center">
-        <div class="col-md-3 justify-content-center">
+<div class="container-fluid py-5 mb-6">
+    <div class="row justify-content-center gap-1"> <!-- Ajoutez gap-3 pour l'espacement entre les cartes -->
+        <div class="col-md-6">
             <div class="card">
                 <img src="" class="card-img-top" alt="Article Image">
-                <div class="card-body d-flex flex-column gap-4">
+                <div class="card-body d-flex flex-column gap-2">
                     <h5 class="card-title">Titre</h5>
                     <p class="card-text text-secondary">Description</p>
                     <a href="#" class="btn btn-dark text-align-center">Cliquez-ici</a>
