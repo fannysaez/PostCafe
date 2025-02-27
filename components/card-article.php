b8d1a97ee5a5b50148c1ca135ea5d104d908c01a
saez.fanny.63@gmail.com <saez.fanny.63@gmail.com>
2025-02-27 13:23:55 +0100

    initiale PostCoffee - blog

--- components/card-article.php
+++ components/card-article.php
@@ -1,12 +1,13 @@
+
 <div class="col-md-3 mb-4">
     <div class="card" style="width: 18rem;">
-        <img src="/assets/img/articles/ <?php echo $article["/assets/img/articles"] ;?>" class="card" alt=<?php echo $article["/assets/img/articles"]; ?>
+        <img src="/assets/img/articles/<?php echo $article['img_cover']; ?>" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 class="card-title"><?php echo $article['title'] ?></h5>
-            <p class="card-text text-secondary"><?php echo $article['description'] ?></p>
+            <h5 class="card-title"><?php echo $article['title']; ?></h5>
+            <p class="card-text text-secondary"><?php echo $article['description']; ?></p>
             <p class="card-description"><?php echo $article['content']; ?></p>
-            <p class="card-creator"><?php echo $article['created_at']; ?></p>
-            <a href="pages/single.php?id=<?php echo $id ?>" class="border btn btn-light">Ouvrir l'article</a>
+            <p class="card-creator"><?php echo date('d/m/Y H:i', strtotime($article['created_at'])); ?></p>
+            <a href="article.php?id=<?php echo $article['id']; ?>" class="border btn btn-light">Ouvrir l'article</a>
         </div>
     </div>
 </div>
